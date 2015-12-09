5de50a92976e02793955d94fa8e67ca298d0661d
Michael Stewart <michael@michael-stewart.com>
2015-12-09 10:12:53 -0800

    Very basic v1 working

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask.ext.api import status, exceptions
+from api_exceptions import InvalidUsage
 app = Flask(__name__)
 
+import json
 import Queue
 q = Queue.Queue()
 
@@ -8,18 +11,29 @@ q = Queue.Queue()
 def hello_world():
     return 'Hello World!'
 
-@app.route('/add')
+@app.route('/add/', methods=['POST'])
 def add():
+    try:
+        data = json.loads(request.data)
+        q.put(data)
+    except ValueError:
+        raise InvalidUsage('Invalid json', status_code=400)
     return ''
 
-@app.route('/get')
+@app.route('/get/', methods=['GET'])
 def get():
-    return ''
+    timeout = int(request.args.get('timeout', 1))
+    try:
+        msg = q.get(timeout=timeout)
+        return jsonify(msg)
+    except Queue.Empty:
+        return '', status.HTTP_404_NOT_FOUND
 
-@app.route('/delete')
-def delete():
-    return ''
-
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
